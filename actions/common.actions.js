abada737620be636bad23d765cd8c8c0b708ad76
Ernst-D <ernst.dzerevanko@gmail.com>
2022-01-13 23:04:55 +0300

    [logout-ui] added logout ui spec

--- actions/common.actions.js
+++ actions/common.actions.js
@@ -1,3 +1,4 @@
+const PW = require("@playwright/test")
 const MainPage = require("../pages/main.page")
 
 module.exports = {
@@ -10,5 +11,14 @@ module.exports = {
         await mainPage.page.fill('input[name="email"]', 'hkosotnvatcoetuzln@kvhrs.com');
         await mainPage.page.fill('input[name="passwd"]', 'password123');    
         await mainPage.page.click('button:has-text("Sign in")');
+    },
+
+    /**
+     * 
+     * @param {PW.Page} page 
+     */
+    async logoutAction(page){
+        await page.locator("[class='logout'] >> text=Sign out").click();
+        await expect(page.locator("a >> text=Sign in")).toBeVisible();  
     }
 }
--- tests/logout.spec.js
+++ tests/logout.spec.js
@@ -1,9 +1,9 @@
 // @ts-check
 const { test, expect} = require('@playwright/test');
-const { loginAction } = require('../actions/common.actions');
+const { loginAction, logoutAction } = require('../actions/common.actions');
 const MainPage = require('../pages/main.page');
 
-test('Logout UI', async ({ page }) => {
+test('Logout UI with predefined user', async ({ page }) => {
     // TODO: move this to the fixture, tests should conatin only logic
     // not the technical implementation
     let mainPage = new MainPage(page);
@@ -16,7 +16,13 @@ test('Logout UI', async ({ page }) => {
     });
     await test.step("Assert personal info", async()=>{
         // Click text=My personal information
-        await page.click('text=My personal information');
+        /**
+         * TODO: add assert for user email, which will comes from User entity
+         */        await page.click('text=My personal information');
         expect(page.url()).toBe('http://automationpractice.com/index.php?controller=identity');
+
+    })
+    await test.step("Logout action", async ()=>{
+       await logoutAction(page);
     })
 });
