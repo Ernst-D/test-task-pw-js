d8e3aee57c0fa5cf23e58b3a7d216c86541a214d
Ernst-D <ernst.dzerevanko@gmail.com>
2022-01-14 11:03:00 +0300

    [api] added api tests

--- tests/api.spec.js
+++ tests/api.spec.js
@@ -1,23 +1,50 @@
 // @ts-check
+const PW = require("@playwright/test");
 const { test, expect} = require('@playwright/test');
 const chance = require("chance").Chance;
 
-const apiUrl = "https://reqres.in/api";
+/**
+ * @type {PW.APIRequestContext}
+ */
+let apiContext;
+test.beforeAll(async ({ playwright }) => {
+    apiContext = await playwright.request.newContext({
+      baseURL: "https://reqres.in"
+    });
+  });
+test.afterAll(async ({ }) => {
+    await apiContext.dispose();
+});
 
-test.use({
-    baseURL:apiUrl
-})
-
-test('Create user', async ({ request }) => {
-    let name = chance().name();
-    let job = chance().word();
-    let response = await request.post('/users',{
-        data:{
+test.describe("API test suite",()=>{
+    test('Create user', async () => {
+        let name = chance().name();
+        let job = chance().word();
+        let payload = {
             "name": name,
             "job": job
-        }
+        };
+    
+        let response = await apiContext.post('/api/users',{
+            data:{
+                ...payload
+            }
+        });
+    
+        expect(response.ok()).toBeTruthy();
+        expect(await response.json()).toHaveProperty("name",payload.name);
     });
-    console.log((await response.json()));
-    console.log(response.status())
-    expect(response.ok()).toBeTruthy();
-});
+    test('Get created user', async () => {
+        let response = await apiContext.get(`/api/users/12`);
+        expect(await response.json()).toHaveProperty("data.email","rachel.howell@reqres.in");
+    });
+    test('Update created user', async () => {
+        let response = await apiContext.patch(`/api/users/2`);
+        expect(response.status()).toBe(200);
+        expect(await response.json()).toHaveProperty("updatedAt");
+    });
+    test("Delete user",async ()=>{
+        let response = await apiContext.delete('/api/users/2');
+        expect(response.status()).toBe(204);
+    })
+})
