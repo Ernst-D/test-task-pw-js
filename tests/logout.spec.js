403343790f3eeac4bf83885c68714f1efe2a2bed
Ernst-D <ernst.dzerevanko@gmail.com>
2022-01-14 10:59:42 +0300

    [logout] refactor logout spec to use fixture

--- tests/logout.spec.js
+++ tests/logout.spec.js
@@ -1,56 +1,46 @@
-// @ts-check
-const { test, expect} = require('@playwright/test');
+const base = require('@playwright/test');
+const { expect } = require('@playwright/test');
 const { loginAction, logoutAction } = require('../actions/common.actions');
+const { predefinedUser, testUrl } = require("../fixtures/logout.fixture");
 const MainPage = require('../pages/main.page');
 
-const testUrl = new URL("http://automationpractice.com/index.php");
 
-test('Logout UI with predefined user', async ({ page }) => {
-    // TODO: move this to the fixture, tests should conatin only logic
-    // not the technical implementation
-    let mainPage = new MainPage(page);
+/**
+ * "context" and "page" fixtures are not supported in beforeAll,
+ * so we extend test object with some predefined conditions
+ */
+const test = base.test.extend({    
+    testUrl: ({},use) =>{
+        use(testUrl);
+    },
+    mainPage: async ({ page }, use) => {
+        const mainPage = new MainPage(page);
 
-    await test.step("Navigate to the main page",async()=>{
-        await page.goto(testUrl.href);
-    });
-    await test.step("Login action",async()=>{
-        await loginAction(mainPage);
-    });
-    await test.step("Assert personal info", async()=>{
-        /**
-         * TODO: add assert for user email, which will comes from User entity
-         */        
-        await page.click('text=My personal information');
-        expect(page.url()).toBe(`${testUrl.href}?controller=identity`);
+        await test.step("Navigate to the main page",async()=>{
+            await mainPage.page.goto(testUrl.href);
+        });
+        await test.step("Login action",async()=>{
+            await loginAction(mainPage);
+        });
+        await test.step("Assert personal info", async()=>{   
+            await page.click('text=My personal information');
+            await expect(page.locator('#email')).toHaveValue(predefinedUser().email);
+        });
+        await use(mainPage);
+}});
 
-    })
+test('Logout UI with predefined user', async ({ mainPage }) => {
+    let page = mainPage.page;
     await test.step("Logout action via UI", async ()=>{
        await logoutAction(page);
        await expect(page.locator("a >> text=Sign in")).toBeVisible();  
     })
 });
 
-test("Logout via API with predifined user", async ({page})=>{
-    let mainPage = new MainPage(page);
-
-    await test.step("Navigate to the main page",async()=>{
-        await page.goto(testUrl.href);
-    });
-    await test.step("Login action",async()=>{
-        await loginAction(mainPage);
-    });
-    await test.step("Assert personal info", async()=>{
-        /**
-         * TODO: add assert for user email, which will comes from User entity
-         */        
-        await page.click('text=My personal information');
-        expect(page.url()).toBe(`${testUrl.href}?controller=identity`);
-
-    })
-    await test.step("Logout action via API", async ()=>{
-        // @ts-ignore
-        page.testUrl = testUrl;
-        await logoutAction(page,"request");
-        await expect(page.locator("a >> text=Sign in")).toBeVisible();  
+test("Logout via API with predifined user", async ({mainPage, testUrl})=>{
+    await test.step("Logout action via request", async ()=>{
+        mainPage.page.testUrl = testUrl;
+        await logoutAction(mainPage.page,"request");
+        await expect(mainPage.page.locator("a >> text=Sign in")).toBeVisible();  
      })
 })
