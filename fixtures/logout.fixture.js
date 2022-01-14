0312a47032458ad4378e0ea9821f1b55f6ff227f
Ernst-D <ernst.dzerevanko@gmail.com>
2022-01-14 10:56:33 +0300

    [logout] added test data

---
+++ fixtures/logout.fixture.js
@@ -0,0 +1,9 @@
+module.exports = {
+    testUrl: new URL("http://automationpractice.com/index.php"),
+    predefinedUser: function(){
+        return {
+            email:"hkosotnvatcoetuzln@kvhrs.com",
+            password:"password123"
+        }
+    }
+}
