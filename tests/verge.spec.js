08b1429ed2b333be0df66149f842d7483775661f
Ernst D <ernst.dzerevanko@gmail.com>
2022-02-21 18:44:44 +0300

    fixed spec

--- tests/verge.spec.js
+++ tests/verge.spec.js
@@ -2,14 +2,7 @@ const { test, expect } = require('@playwright/test');
 
 test('test', async ({ page }) => {
 
-  // Go to https://www.google.com/search?q=verge&oq=verge&aqs=chrome..69i57.760j0j2&sourceid=chrome&ie=UTF-8
-  await page.goto('https://www.google.com/search?q=verge&oq=verge&aqs=chrome..69i57.760j0j2&sourceid=chrome&ie=UTF-8');
-
-  // Click text=The Vergehttps://www.theverge.comhttps://www.theverge.com >> h3
-  await page.locator('text=The Vergehttps://www.theverge.comhttps://www.theverge.com >> h3').click();
-  await expect(page).toHaveURL('https://www.theverge.com/');
-
-  // Click img >> nth=0
-  await page.locator('img').first().click();
-  await expect(page).toHaveURL('https://www.theverge.com/2022/2/21/22944025/capcom-street-fighter-6-announced-capcom-fighting-collection');
+  await page.goto('https://playwright.dev/');
+  const title = page.locator('.navbar__inner .navbar__title');
+  await expect(title).toHaveText('Playwright');
 });
